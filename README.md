572f68b90c347af1d321d8d8fa65e78d6466abb5
Rimon <114850164+rimonenaffi@users.noreply.github.com>
2023-09-10 09:04:32 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+## Black-Holes - ITI Project
+
+![Black Holes](assets/img/project-capture.jpeg)
+
+## Main Idea of The Project
+**It's a website describing the historical story of the black holes in our galaxy made by Rimone Naffi**
+
+*Note: We used **HTML, CSS, javaScript***
+
+
+*README Last Updated: **9/September/2023.***
+
+
+## Project Team
+* [Riomne Naffi](https://www.linkedin.com/in/rimone/)
+
+<p align="center">Black-Holes, Made with :heart:&:coffee:</p>
+<p align="center">Seeking for Knowledge and Wisdom.</p>
+
---
+++ script.js
@@ -0,0 +1,13 @@
+
+ function downloadFile2(){window.open(" assets/img/0122.png")}
+         function downloadFile1(){window.open("assets/img/hooole.png")}
+         function downloadFile3() { window.open("assets/img/841.png") }
+            function downloadFile4() { window.open("assets/img/black-hole-artwork-royalty-free-illustration-1616081355_.png") }
+            function downloadFile5() { window.open("assets/img/black-holes.png") }
+            function downloadFile6() { window.open("assets/img/HFpPPGTdiV5oaTXNzdmKMG-1200-80.png") }
+         function downloadFile7() { window.open("assets/img/r69_722_4857_3806_w5906_h3804_fmax.png") }
+            function downloadFile8() { window.open("assets/img/fractal-1280110_640.png") }
+            function downloadFile9() { window.open("assets/img/STScI-01FMN37V4VGHDJBNEJYZ5RAY70.png") }
+            function downloadFile10() { window.open("assets/img/simulated_bh.png") }
+            function downloadFile11() { window.open("assets/img/Photon-Ring_2560_Lede.png") }
+            function downloadFile12() { window.open("assets/img/owaa8gdm.png") }
